39fe9bfd55c96ec5b9cff675567d209c04061be4
baha-elfidha <bahaeddine.elfidha@esprit.tn>
2022-12-06 11:30:37 +0100

    ajout

---
+++ connection.cpp
@@ -0,0 +1,26 @@
+#include "connection.h"
+
+Connection::Connection()
+{
+
+}
+
+bool Connection::createconnect()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("source_projet2a");
+//db.setUserName("baha");//inserer nom de l'utilisateur
+//db.setPassword("admin");//inserer mot de passe de cet utilisateur
+db.setDatabaseName("source_Projet2A");
+db.setUserName("system");//inserer nom de l'utilisateur
+db.setPassword("01062001");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+
+
+
+
+
+    return  test;
+}
